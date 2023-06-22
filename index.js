2ae4597b4a5b90cb0ea5de4f21a1505ef06e038d
njagiiii <grace.makena@student.moringaschool.com>
2023-06-22 12:37:43 -0400

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // Write your code here!
+const main = document.getElementById('main')
+  main.remove();
+
+  const body = document.body
+
+   const newHeader = document.createElement('h1')
+   newHeader.id ='victory'
+   newHeader.innerHTML ='Grace Makena is the champion'
+   body.appendChild(h1);
